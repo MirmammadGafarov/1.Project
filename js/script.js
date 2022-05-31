c7f2d335f962d1302fec8baa3fb249d19acf5381
mirmammad.gafarov@gmail.com <mirmammad.gafarov@gmail.com>
2022-05-31 17:28:16 +0400

    First Project

---
+++ js/script.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+    var whenChange = function() {
+      var CW = document.body.clientWidth;
+      if (CW <= 992) {
+        $('.navigate').addClass('col-md-12');
+        $('.navigate').removeClass('col-md-5');
+       
+      } else if (CW > 992) {
+        $('.navigate').removeClass('col-md-12');
+        $('.navigate').addClass('col-md-5');
+
+      };
+    };
+    $(window).resize(function(){
+        whenChange();
+    });
+    //Fire it when the page first loads:
+    whenChange();
+  });
